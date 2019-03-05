c37748c6bc61e6dc4b746846bc4e03edc1e6bfe6
Administrator <mgiannini@hardindd.com>
2019-03-04 22:24:17 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation, private http: HttpClient) {
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+       console.log(resp.coords.latitude);
+       console.log(resp.coords.longitude);
+       var apiCall = "http://api.openweathermap.org/data/2.5/weather?lat="+resp.coords.latitude+"&lon="+resp.coords.longitude+"&APPID=664bfb683ddf553092f072ef0a65abcf"
+       console.log(apiCall);
+       this.http.get(apiCall).subscribe((response) => {
+         console.log(response);
+      });
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
 
   }
 
